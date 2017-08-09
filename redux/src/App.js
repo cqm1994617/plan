bb4314b8badeac1a6c76bca492b55f5a1fe2cc28
绯色流火 <1025760508@qq.com>
2017-08-09 14:06:57 +0800

    redux test

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,21 +1,35 @@
 import React from 'react';
+import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {add, setNumber, init} from './action/action';
+import Main from './components/Main';
 
-@connect((state) => state)
+/*
+import * as action from './action/action';
+ (dispatch) => ({
+ ...bindActionCreators(action, dispatch)
+ })
+ */
+
+
+
+@connect((state) => state, (dispatch) => ({
+  add: () => dispatch(add()),
+  setNumber: (z) => dispatch(setNumber(z)),
+  init: () => dispatch(init(15))
+}))
 export default class App extends React.Component {
 
   render() {
-    console.log(this.props)
-    const {dispatch, reducer, reducer2} = this.props;
-    console.log(reducer === reducer2)
+    console.log(this.props);
+    const {dispatch, add, setNumber, init} = this.props;
     return (
       <div>
         {this.props.reducer.num} <br />
         {this.props.reducer2.data}
-        <button onClick={() => dispatch(add())}>+1</button>
-        <button onClick={() => dispatch(setNumber(100))}>setNumber = 100</button>
-        <button onClick={() => dispatch(init(100))}>INIT</button>
+        <button onClick={() => add()}>+1</button>
+        <button onClick={() => setNumber(120)}>setNumber = 120</button>
+        <button onClick={() => init()}>INIT</button>
       </div>
     );
   }
